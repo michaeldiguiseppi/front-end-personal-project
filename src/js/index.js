e1e50fabe66debe22a182895653cab97229164cc
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-01-30 16:25:50 -0700

    Fixed buttons to only show up on hover of the images

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
 $(document).on('ready', function() {
-
+    $('.imgButton').hover(function() {
+        var button = $(this).find('button');
+        button.toggleClass('hiddenButton');
+    });
 });
