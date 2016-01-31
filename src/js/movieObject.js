c35ffa776da0b1478ad4e348f82ca50e9046d035
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-01-30 17:01:20 -0700

    Added removal button to collection page

--- src/js/movieObject.js
+++ src/js/movieObject.js
@@ -50,7 +50,7 @@ function addDataFromLocalStorageToDom() {
     $('.myCollection').empty();
     var allMovies = JSON.parse(localStorage.getItem('movies'));
     allMovies.forEach(function(obj) {
-        $('.movieCollection').append('<div class="panel panel-info"><div class="panel-heading"><h3 class="panel-title">'+obj.Title+'</h3></div><div class="panel-body"><img class="posterImages" src="'+obj.Poster+'"><ul class="list-group col-lg-8 col-lg-offset-4 col-md-8 col-md-offset-4 col-sm-8 col-sm-offset-4"><li class="list-group-item">Release Year: '+obj.Year+'</li><li class="list-group-item">Genre: '+obj.Genre+'</li><li class="list-group-item">IMDB Rating: '+obj.Rating+'</li><li class="list-group-item">Media Type: '+obj.Type+'</li></ul></div></div>');
+        $('.movieCollection').append('<div class="panel panel-info"><div class="panel-heading"><h3 class="panel-title">'+obj.Title+'</h3></div><div class="panel-body"><img class="posterImages" src="'+obj.Poster+'"><ul class="list-group col-lg-8 col-lg-offset-4 col-md-8 col-md-offset-4 col-sm-8 col-sm-offset-4"><li class="list-group-item">Release Year: '+obj.Year+'</li><li class="list-group-item">Genre: '+obj.Genre+'</li><li class="list-group-item">IMDB Rating: '+obj.Rating+'</li><li class="list-group-item">Media Type: '+obj.Type+'</li><br><br><br><button type="submit" class="btn btn-danger btn-block halfSize">Remove From My Collection</button></ul></div></div>');
     });
 }
 
